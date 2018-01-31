8de9f1a52fe96b5aaf8896134393d846c1bc680f
Süleyman Yaman <suleymanyamantr@gmail.com>
2018-01-31 14:46:21 +0300

    frst

---
+++ SRVR.py
@@ -0,0 +1,56 @@
+import socket
+from threading import Thread
+
+def accept_incoming_connections():
+    while True:
+        client, client_adress = SERVER.accept()
+        client.send("Welcome to the NSA. Your messages are secured by encryption. Choose a nickname for yourself. For quitting, type EXIT N.S.A".encode())
+        print("%s:%s has connected" % client_adress)
+        #adresses[client]=client_adress
+        Thread(target=handle_client, args=(client,)).start()
+
+def handle_client(client):
+    name = client.recv(1024).decode()
+    msg = "{} is online".format(name)
+    broadcast(msg.encode())
+    clients.append(client)
+
+
+    while True:
+        msg = client.recv(1024)
+        print(msg)
+        if msg.decode() != "EXIT N.S.A":
+            broadcast(msg, name+": ")
+
+        else:
+            client.send("You're leaving N.S.A".encode())
+            client.close()
+            clients.remove(client)
+            broadcast("{} is offline".format(name).encode())
+            break
+
+def broadcast(msg, prefix=""):
+    for sock in clients:
+        sock.send(bytes(prefix, "utf-8")+ msg)
+
+
+clients=[]
+adresses={}
+
+HOST = "192.168.1.34"
+PORT = 5000
+ADDR = (HOST,PORT)
+
+SERVER = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+SERVER.bind(ADDR)
+
+if __name__== "__main__":
+    SERVER.listen(5)
+    print("Waiting for connection.....")
+    ACCEPT_THREAD = Thread(target=accept_incoming_connections)
+    ACCEPT_THREAD.start()
+
+
+
+
+#listen, blocking-non blocking
