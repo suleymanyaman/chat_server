8befa4b537a7887e61949c75671e4791571db326
Süleyman Yaman <33877588+suleymanyaman@users.noreply.github.com>
2019-02-04 23:28:52 +0300

    Update SRVR.py

--- SRVR.py
+++ SRVR.py
@@ -1,42 +1,111 @@
 import socket
 from threading import Thread
+import random
+from faker import Faker
+
+
+#fake=Faker("en_US")
+
+
+
+def get_key(dict, value):
+    return list(dict.keys())[list(dict.values()).index(value)]
+
+
 
 def accept_incoming_connections():
     while True:
         client, client_adress = SERVER.accept()
-        client.send("Your messages are secured by encryption. Choose a nickname for yourself.".encode())
+        client.send("Welcome to Chit-Chat !. Press 'SHUFFLE' to connect someone".encode())
         print("%s:%s has connected" % client_adress)
-        #adresses[client]=client_adress
         Thread(target=handle_client, args=(client,)).start()
 
+
+
 def handle_client(client):
-    name = client.recv(1024).decode()
-    welcome = 'Welcome %s! If you ever want to quit, type {EXIT} to exit.' % name
-    client.send(bytes(welcome, "utf8"))
-    msg = "{} is online".format(name)
-    broadcast(msg.encode())
-    clients.append(client)
+    #Make sure that every client has a distinct id
+    global target_client
+    while 1:
+        client_id = client.recv(1024).decode()
+        if client_id not in list(clients.values()):
+            clients[client]=client_id
+            print(client_id)
+            status[client_id]="AVAILABLE"
+            break
+        else:
+            continue
+
+    #client.send("Your nickname is {}".format(client_id).encode())
 
 
-    while True:
-        msg = client.recv(1024)
-        if msg.decode() != "EXIT":
-            broadcast(msg, name+": ")
+
+    #Make sure that every client has a distinct peer
+
+
+    while 1:
+        print(len(clients))
+        msg = client.recv(1024).decode()
+        print(msg)
+        if msg == "?pRG=gmxHD74cEm":
+            if client_id in list(match.keys())+list(match.values()):
+                try:
+                    del match[client_id]
+                    get_key(clients, target_client).send("Your partner has left the chat :(".encode())
+                    status[target_client]="AVAILABLE"
+
+
+
+                except KeyError:
+                    del match[get_key(match, client_id)]
+                    get_key(clients, target_client).send("Your partner has left the chat :(".encode())
+                    status[target_client] = "AVAILABLE"
+
+            target_client = random.choice(list(clients.values()))
+            if client_id!=target_client and status[target_client]=="AVAILABLE":
+                match[client_id] = target_client
+                status[client_id] = "FULL"
+                status[target_client] = "FULL"
+                client.send("You have been connected to someone!".encode())
+                get_key(clients,target_client).send(
+                    "You have been connected to someone!".encode())
+
+
+            else:
+                client.send("We couldn't find a match for you, try again".encode())
+                continue
+
+        elif msg == "4t7w!z%C":
+            client.close()
+            try:
+                get_key(clients, target_client).send("Your partner has left the chat!".encode())
+                status[target_client] = "AVAILABLE"
+            finally:
+                del clients[client]
+                del status[client_id]
+                print(len(clients))
+                break
+
 
         else:
-            client.send("You're leaving...".encode())
-            client.close()
-            clients.remove(client)
-            broadcast("{} is offline".format(name).encode())
-            break
+            frm = clients[client]
+            try:
+                to = match[frm]
+            except KeyError:
+                to = get_key(match, frm)
 
-def broadcast(msg, prefix=""):
-    for sock in clients:
-        sock.send(bytes(prefix, "utf-8")+ msg)
+            
+            to_client = get_key(clients, to)
+            to_client.send((client_id+":"+msg).encode("utf-8"))
+            client.send(("Me"+":"+msg).encode("utf-8"))
 
 
-clients=[]
-adresses={}
+clients={}
+status={}
+match={}
+
+
+
+
 
 HOST = "0.0.0.0"
 PORT = 5000
@@ -50,8 +119,3 @@ if __name__== "__main__":
     print("Waiting for connection.....")
     ACCEPT_THREAD = Thread(target=accept_incoming_connections)
     ACCEPT_THREAD.start()
-
-
-
-
-#listen, blocking-non blocking
